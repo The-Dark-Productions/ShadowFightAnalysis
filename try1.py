fc4140f8a4a57a7ce999b1867d863395411db3b7
Chinmay Shah <44554197+cross034@users.noreply.github.com>
2019-09-14 15:47:26 +0530

    Update try1.py
    Used PIL instead of open-cv so that the code can be run on pydroid.

--- try1.py
+++ try1.py
@@ -1,47 +1,47 @@
 # Basic script to get player and enemy health from screenshot
 
-import cv2
-import numpy as np
 import math
+from PIL import Image
 
-# Total health pixels = 378
-full_health = 378
-
-# Health bar end pixels
-player_health_full = (126, 749)
-player_health_zero = (126, 1126)
-enemy_health_full = (126, 1666)
-enemy_health_zero = (126, 1289)
-
+# Total health pixels = 396
+full_health = 396
+# Health bar ends
+player_health_full = (749, 126)
+player_health_zero = (1126, 126)
+enemy_health_full = (1666, 126)
+enemy_health_zero = (1289, 126)
 # Health bar pixel lists
-player_health_pixels = zip(378*[126], range(1126, 748))
-enemy_health_pixels = zip(378*[126], range(1289, 1667))
+player_health_pixels = zip(range(1126, 748), 396*[126])
+enemy_health_pixels = zip(range(1289, 1667), 396*[126])
 
 
 # The R value of pixels in health bar which indicate hp greater than current hp is 49
 def get_player_health():
     for pixel in player_health_pixels:
-        pixel_r = img.item(*pixel, 2)
+        pixel_r = px[pixel[0], pixel[1]][0]
         if pixel_r is 49:
-            health = math.fabs(pixel[1] - player_health_zero[1])
+            health = math.fabs(pixel[0] - player_health_zero[0])
             return health
     return full_health
 
 
 def get_enemy_health():
     for pixel in enemy_health_pixels:
-        pixel_r = img.item(*pixel, 2)
+        pixel_r = px[pixel[0], pixel[1]][0]
         if pixel_r is 49:
-            health = math.fabs(pixel[1] - enemy_health_zero[1])
+            health = math.fabs(pixel[0] - enemy_health_zero[0])
             return health
     return full_health
 
 
-img = cv2.imread("Screenshot.png")
+img = Image.open("Screenshot1.png")
+px = img.load()
 
 enemy_health = get_enemy_health()
+print("\nEnemy health:")
 print(enemy_health)
 print(enemy_health/full_health * 100)
 player_health = get_player_health()
+print("\nPlayer health:")
 print(player_health)
 print(player_health/full_health * 100)
